67a0092f8b74861a6a88f1ad225f14b2550b5c78
lizloang <liz.loang@gmail.com>
2022-08-29 18:04:00 -0500

    Add welcome style whitout correct logo

--- src/main.js
+++ src/main.js
@@ -5,34 +5,40 @@
 // myFunction();
 
 function welcome() {
+  // Buttons Container
   const divContainer = document.createElement('div');
-  const container = document.createElement('div');
-  divContainer.setAttribute('class', 'default');
-  const logo = document.createElement('img');
-  logo.src = './img/logo.png';
-  const buttonsContainer = document.querySelector('main').appendChild(container);
-  buttonsContainer.appendChild(divContainer);
+  const buttonsContainer = document.querySelector('main').appendChild(divContainer);
   buttonsContainer.setAttribute('class', 'buttons-container');
 
-  const divDefault = document.querySelector('.default');
-  divDefault.appendChild(logo);
+  // Logo 
+  const logo = document.createElement('img');
+  logo.src = './img/logo.png';
+  buttonsContainer.appendChild(logo);
 
+  // Text
   const text = document.createElement('p');
   text.setAttribute('class', 'welcome-text');
   text.innerHTML = 'Join us and share your healthy life style';
-  divDefault.appendChild(text);
-
+  buttonsContainer.appendChild(text);
+ 
+  //Login
   const loginBtn = document.createElement('button');
-  loginBtn.setAttribute('class', 'login-button');
+  loginBtn.setAttribute('class', 'button');
   loginBtn.setAttribute('name', 'login');
   loginBtn.innerHTML = 'Login';
-  divDefault.appendChild(loginBtn);
+  buttonsContainer.appendChild(loginBtn);
 
+  //Register
   const registerBtn = document.createElement('button');
-  registerBtn.setAttribute('class', 'register-button');
-  registerBtn.setAttribute('name', 'login');
-  registerBtn.innerHTML = 'Login';
-  divDefault.appendChild(registerBtn);
+  registerBtn.setAttribute('class', 'button');
+  registerBtn.setAttribute('name', 'register');
+  registerBtn.innerHTML = 'Register';
+  buttonsContainer.appendChild(registerBtn);
+
+  //Footer Div
+  const divFooter = document.createElement('div');
+  divFooter.setAttribute('class', 'welcome-footer');
+  document.querySelector('footer').appendChild(divFooter);
 }
 
 welcome();

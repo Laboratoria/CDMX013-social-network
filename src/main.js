cd2ca251e8ecc274a8d0b304600bec7167800cf6
lizloang <liz.loang@gmail.com>
2022-08-30 09:38:41 -0500

    Add welcome style whitout correct logo

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ function welcome() {
   const buttonsContainer = document.querySelector('main').appendChild(divContainer);
   buttonsContainer.setAttribute('class', 'buttons-container');
 
-  // Logo 
+  // Logo
   const logo = document.createElement('img');
   logo.src = './img/logo.png';
   buttonsContainer.appendChild(logo);
@@ -20,22 +20,22 @@ function welcome() {
   text.setAttribute('class', 'welcome-text');
   text.innerHTML = 'Join us and share your healthy life style';
   buttonsContainer.appendChild(text);
- 
-  //Login
+
+  // Login
   const loginBtn = document.createElement('button');
   loginBtn.setAttribute('class', 'button');
   loginBtn.setAttribute('name', 'login');
   loginBtn.innerHTML = 'Login';
   buttonsContainer.appendChild(loginBtn);
 
-  //Register
+  // Register
   const registerBtn = document.createElement('button');
   registerBtn.setAttribute('class', 'button');
   registerBtn.setAttribute('name', 'register');
   registerBtn.innerHTML = 'Register';
   buttonsContainer.appendChild(registerBtn);
 
-  //Footer Div
+  // Footer Div
   const divFooter = document.createElement('div');
   divFooter.setAttribute('class', 'welcome-footer');
   document.querySelector('footer').appendChild(divFooter);

0fed0ed6a6015d71952cb2f2adcb7f851d776531
Fer Lanuz <ferlaranuz@gmail.com>
2022-09-12 15:13:03 -0500

    add logout icon to home view

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,19 @@
 /* eslint-disable space-before-blocks */
 export function home(){
-  const homeText = document.createElement('h1');
-  homeText.textContent = 'home';
-
-  return homeText;
+  // Father container
+  const fatherOfAll = document.createElement('div');
+  // Create img for home view
+  const homeImg = document.createElement('img');
+  homeImg.src = '../img/confetti.jpg';
+  homeImg.setAttribute('class', 'home-img');
+  // Create text for home view
+  const welcomeTxt = document.createElement('h2');
+  welcomeTxt.textContent = 'Welcome';
+  welcomeTxt.setAttribute('class', 'welcome-title');
+  // Create logout icon
+  const logOut = document.createElement('img');
+  logOut.src = '../img/switch.png';
+  logOut.setAttribute('class', 'log-out');
+  fatherOfAll.append(homeImg, welcomeTxt, logOut);
+  return fatherOfAll;
 }

c9843667d747dbd8ed6d36f0de48d5e66d3ffef8
atzinmartinez <atzinrgm@gmail.com>
2022-09-29 13:23:00 -0500

    comentarios no valido

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,66 +1,69 @@
+/* eslint-disable max-len */
 import { onNavigate } from '../main.js';
 import { guardarUssuario } from '../firebase/auth.js';
 
 export const Login = () => {
-    const div = document.createElement('div');
-    const titlepost = document.createElement('h1');
-    const title = document.createElement('h3');
-    const button = document.createElement('button');
-    const buttonBack = document.createElement('button');
-    const inputEmail = document.createElement('input');
-    const inputPassword = document.createElement('input');
-    const recuerdame = document.createElement('p');
-    const buttongoogle = document.createElement('button');
-    const forgotpassword = document.createElement('p');
-    const line = document.createElement('hr');
-    const notienescuenta = document.createElement('p');
-    const buttonnuevacuenta = document.createElement('button');
+  const div = document.createElement('div');
+  const titlepost = document.createElement('h1');
+  const title = document.createElement('h3');
+  const button = document.createElement('button');
+  const buttonBack = document.createElement('button');
+  const inputEmail = document.createElement('input');
+  const inputPassword = document.createElement('input');
+  const recuerdame = document.createElement('p');
+  const buttongoogle = document.createElement('button');
+  const forgotpassword = document.createElement('p');
+  const line = document.createElement('hr');
+  const notienescuenta = document.createElement('p');
+  const buttonnuevacuenta = document.createElement('button');
 
-    titlepost.textContent = 'Postalk';
-    titlepost.classList = ('postalklog');
-    div.classList.add('container-login');
-    button.textContent = 'Inicia Sesión';
-    button.classList = ('buttonlogin');
-    buttonBack.textContent = 'Regresa';
-    buttonBack.classList.add('buttonback');
-    title.textContent = 'Inicia sesión para acceder a tu cuenta';
-    title.classList = ('iniciasesion');
-    inputEmail.placeholder = 'Correo electronico';
-    inputEmail.classList = ('emaillogin');
-    inputPassword.placeholder = 'Contraseña';
-    inputPassword.classList = ('passwordlogin');
-    recuerdame.textContent = 'Recordarme';
-    recuerdame.classList = ('recuerdame');
-    buttongoogle.textContent = '';
-    buttongoogle.classList = ('buttongoogle');
-    forgotpassword.textContent = '¿Olvisdaste la constraseña?';
-    forgotpassword.classList = ('forgotpassword');
-    line.classList = ('line');
-    notienescuenta.textContent = '¿No tienes cuenta?';
-    notienescuenta.classList = ('textcuenta');
-    buttonnuevacuenta.textContent = 'Registrar';
-    buttonnuevacuenta.classList = ('buttonnuevacuenta');
+  titlepost.textContent = 'Postalk';
+  titlepost.classList = ('postalklog');
+  div.classList.add('container-login');
+  button.textContent = 'Inicia Sesión';
+  button.classList = ('buttonlogin');
+  buttonBack.textContent = 'Regresa';
+  buttonBack.classList.add('buttonback');
+  title.textContent = 'Inicia sesión para acceder a tu cuenta';
+  title.classList = ('iniciasesion');
+  inputEmail.placeholder = 'Correo electronico';
+  inputEmail.classList = ('emaillogin');
+  inputPassword.placeholder = 'Contraseña';
+  inputPassword.classList = ('passwordlogin');
+  recuerdame.textContent = 'Recordarme';
+  recuerdame.classList = ('recuerdame');
+  buttongoogle.textContent = '';
+  buttongoogle.classList = ('buttongoogle');
+  forgotpassword.textContent = '¿Olvisdaste la constraseña?';
+  forgotpassword.classList = ('forgotpassword');
+  line.classList = ('line');
+  notienescuenta.textContent = '¿No tienes cuenta?';
+  notienescuenta.classList = ('textcuenta');
+  buttonnuevacuenta.textContent = 'Registrar';
+  buttonnuevacuenta.classList = ('buttonnuevacuenta');
 
-    buttonBack.addEventListener('click', () => {
-        onNavigate('/');
-    });
-    button.addEventListener('click', () => {
-        guardarUssuario('carlos@carlos.com', '123456').then((userCredential) => {
-            // Signed in
-            console.log('ya se creo tu user');
-            // ...
-        })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log('Nel Karnal no puedes', errorMessage);
-                // ..
-            });
-    });
+  buttonBack.addEventListener('click', () => {
+    onNavigate('/');
+  });
+  button.addEventListener('click', () => {
+    guardarUssuario('carlos@carlos.com', '123456').then((userCredential) => {
+      // Signed in
+      console.log('ya se creo tu user');
+      // ...
+    })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('Nel Karnal no puedes', errorMessage);
+        // ..
+      });
 
-    div.append(titlepost, recuerdame, notienescuenta, line, forgotpassword, buttongoogle, title, inputEmail, inputPassword, button, buttonBack, buttonnuevacuenta);
+      
+  });
 
-    return div;
+  div.append(titlepost, recuerdame, notienescuenta, line, forgotpassword, buttongoogle, title, inputEmail, inputPassword, button, buttonBack, buttonnuevacuenta);
+
+  return div;
 };
 
 // modularizar el codigo
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,45 +1,52 @@
 import { onNavigate } from '../main.js';
 
 export const Register = () => {
-    const div = document.createElement('div');
-    const title = document.createElement('h2');
-    const button = document.createElement('button');
-    const buttonBack = document.createElement('button');
-    const inputEmail = document.createElement('input');
-    const inputPass = document.createElement('input');
-    const descriptionRegister = document.createElement('h3');
-    const inputConfirm = document.createElement('input');
-    const buttongoogler = document.createElement('button');
-    const liner = document.createElement('hr');
+  const div = document.createElement('div');
+  const title = document.createElement('h2');
+  const buttonNewRegister = document.createElement('button');
+  const buttonBack = document.createElement('button');
+  const inputEmail = document.createElement('input');
+  const inputPass = document.createElement('input');
+  const descriptionRegister = document.createElement('h3');
+  const inputConfirm = document.createElement('input');
+  const buttongoogler = document.createElement('button');
+  const liner = document.createElement('hr');
 
-    div.classList.add('container-register');
-    button.textContent = 'Crear cuenta';
-    buttonBack.textContent = 'Menú de Inicio';
-    buttonBack.classList = ('buttonBack');
-    title.textContent = 'Postalk';
-    title.classList = ('postalkr');
-    descriptionRegister.textContent = 'Crea una cuenta';
-    descriptionRegister.classList = ('descriptionr');
-    inputEmail.textContent = ('input');
-    inputEmail.classList = ('inputEmail');
-    inputPass.textContent = ('input');
-    inputPass.classList = ('inputPass');
-    inputConfirm.textContent = ('input');
-    inputConfirm.classList = ('inputConfirm');
-    inputEmail.placeholder = 'Email';
-    inputPass.placeholder = 'Contraseña';
-    inputConfirm.placeholder = 'Confirmar contraseña';
-    button.textContent = 'Registrar';
-    button.classList = ('buttonr');
-    buttongoogler.textContent = '';
-    buttongoogler.classList = ('buttongoogle');
-    liner.classList = ('liner');
+  div.classList.add('container-register');
+  buttonBack.textContent = 'Menú de Inicio';
+  buttonBack.classList = ('buttonBack');
+  title.textContent = 'Postalk';
+  title.classList = ('postalkr');
+  descriptionRegister.textContent = 'Crea una cuenta';
+  descriptionRegister.classList = ('descriptionr');
+  inputEmail.textContent = ('input');
+  inputEmail.classList = ('inputEmail');
+  inputPass.textContent = ('input');
+  inputPass.classList = ('inputPass');
+  inputConfirm.textContent = ('input');
+  inputConfirm.classList = ('inputConfirm');
+  inputEmail.placeholder = 'Email';
+  inputPass.placeholder = 'Contraseña';
+  inputConfirm.placeholder = 'Confirmar contraseña';
+  buttonNewRegister.textContent = 'Registrar';
+  buttonNewRegister.classList = ('buttonr');
+  buttongoogler.textContent = '';
+  buttongoogler.classList = ('buttongoogle');
+  liner.classList = ('liner');
 
-    buttonBack.addEventListener('click', () => {
-        onNavigate('/');
-    });
+  document.getElementsByClassName('inputEmail').value;
 
-    div.append(title, liner, buttongoogler, descriptionRegister, buttonBack, inputEmail, inputPass, inputConfirm, button, buttonBack);
+  /*buttonNewRegister.addEventListener ('click' ) , () => {
+ console.log("Hola");
+  }*/
+  buttonBack.addEventListener('click', () => {
+    onNavigate('/');
 
-    return div;
+
+  });
+
+  // eslint-disable-next-line max-len
+  div.append(title, liner, buttongoogler, descriptionRegister, buttonBack, inputEmail, inputPass, inputConfirm, buttonNewRegister, buttonBack);
+
+  return div;
 };

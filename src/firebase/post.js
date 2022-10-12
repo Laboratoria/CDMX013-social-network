2a6f33554cca02b17663f24596c968f1f7103143
Elsy Carolina Zambrano <108857263+ElsyCaro@users.noreply.github.com>
2022-10-12 17:43:32 -0500

    Revert "boton borrar funcional"

--- src/firebase/post.js
+++ src/firebase/post.js
@@ -1,33 +1,34 @@
 import {
-  collection, onSnapshot, addDoc, getFirestore, deleteDoc, doc,
+  collection, onSnapshot, addDoc, getFirestore,
 } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js';
+import { app } from './config.js';
 
 import {
   getAuth, onAuthStateChanged,
 } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
 
-import { app } from './config.js';
-
 const auth = getAuth(app);
 
+
 const db = getFirestore(app);
-let uid = '';
+let uid = "";
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    uid = user.uid;
-    // console.log(uid);
+     uid = user.uid;
+    //console.log(uid);
     // ...
   } else {
   }
 });
+
 
 export const savePost = async (textPost) => {
   const docRef = await addDoc(collection(db, 'post'), {
     mensaje: textPost,
     uid,
   });
-  // console.log('Document written with ID: ', docRef.id);
+  //console.log('Document written with ID: ', docRef.id);
 };
 // firestore es una base de datos no relacional
 //
@@ -38,9 +39,6 @@ export const getPost = (callback) => {
   );
 };
 
-export const deletePost = async (id) => {
-  await deleteDoc(doc(db, 'post', id));
-};
 // todo lo del CRUD aquí
 
-// export const getPost = (callback) => onSnapshot (collection(db,'post'), callback);
+//export const getPost = (callback) => onSnapshot (collection(db,'post'), callback);
